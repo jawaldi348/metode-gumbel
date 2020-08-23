210d51086e9c1945be818c5aae829e137520845f
jawaldi348 <46097155+jawaldi348@users.noreply.github.com>
2020-08-23 23:54:29 +0700

    update database

--- database/db_curahhujan.sql
+++ database/db_curahhujan.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Ultimate v12.4.3 (64 bit)
-MySQL - 5.6.17 : Database - db_curahhujan
+MySQL - 5.7.24 : Database - db_curahhujan
 *********************************************************************
 */
 
@@ -9,26 +9,11 @@ MySQL - 5.6.17 : Database - db_curahhujan
 /*!40101 SET SQL_MODE=''*/;
 
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_curahhujan` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
 USE `db_curahhujan`;
-
-/*Table structure for table `curah_hujan` */
-
-DROP TABLE IF EXISTS `curah_hujan`;
-
-CREATE TABLE `curah_hujan` (
-  `id_curah` int(11) NOT NULL AUTO_INCREMENT,
-  `stasiun_curah` int(11) DEFAULT NULL,
-  `tahun_curah` int(11) DEFAULT NULL,
-  `jumlah_curah` double DEFAULT NULL,
-  PRIMARY KEY (`id_curah`),
-  KEY `stasiun_curah` (`stasiun_curah`),
-  CONSTRAINT `curah_hujan_ibfk_1` FOREIGN KEY (`stasiun_curah`) REFERENCES `stasiun` (`id_stasiun`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
 
 /*Data for the table `curah_hujan` */
 
@@ -54,16 +39,6 @@ insert  into `curah_hujan`(`id_curah`,`stasiun_curah`,`tahun_curah`,`jumlah_cura
 (21,2,2013,92),
 (22,2,2014,60);
 
-/*Table structure for table `stasiun` */
-
-DROP TABLE IF EXISTS `stasiun`;
-
-CREATE TABLE `stasiun` (
-  `id_stasiun` int(11) NOT NULL AUTO_INCREMENT,
-  `nama_stasiun` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id_stasiun`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-
 /*Data for the table `stasiun` */
 
 insert  into `stasiun`(`id_stasiun`,`nama_stasiun`) values 
@@ -71,6 +46,5 @@ insert  into `stasiun`(`id_stasiun`,`nama_stasiun`) values
 (2,'Tanjung Pati');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
